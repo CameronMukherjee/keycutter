81ae6f6e17682dfa039848d5e1aa1448e4ac744b
Cameron Mukherjee <cameron@hexploits.com>
2022-05-14 00:38:24 +0100

    added fetching all users and operations queries for event logs

--- src/main/resources/db/migration/V003__event_log.sql
+++ src/main/resources/db/migration/V003__event_log.sql
@@ -2,8 +2,9 @@ CREATE TABLE IF NOT EXISTS kc_event_log
 (
     sid         bigserial primary key,
     uid         uuid unique not null,
-    kc_user_sid bigserial references kc_user (sid),
+    kc_user_sid bigserial references kc_user (sid) on delete cascade,
     event_type  event_type  not null,
     system_env  varchar(20) not null,
+    metadata    varchar(1000),
     created_at  timestamptz default now()
 );
