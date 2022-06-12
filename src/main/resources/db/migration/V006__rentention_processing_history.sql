0abb0e90d93fcffb2e9ade0abd87aa0b86f7718a
Cameron Mukherjee <cameron@hexploits.com>
2022-06-12 14:37:55 +0100

    retention processing log added

---
+++ src/main/resources/db/migration/V006__rentention_processing_history.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS kc_retention_processing_log
+(
+    sid                bigserial primary key,
+    uid                uuid unique not null,
+    log_type           varchar     not null,
+    entities_processed int         not null,
+    strategy           varchar     not null,
+    created_at         timestamptz not null
+);
+
+CREATE VIEW retention_entity_count AS
+SELECT log_type, strategy, sum(entities_processed)
+FROM kc_retention_processing_log
+GROUP BY log_type, strategy;
